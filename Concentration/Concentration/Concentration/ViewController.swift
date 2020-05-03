6140f11fb9331b896747867afac59b82d6b000d8
Joshua McCord <jmccord34@gatech.edu>
2020-05-03 15:27:07 -0400

    Up to date

--- Concentration/Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/Concentration/ViewController.swift
@@ -10,17 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     
-    var flipCount = 0 { didSet { flipCountLabel.text = "Flips: \(flipCount)" } }
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count + 1) / 2
+    }
     
-    @IBOutlet weak var flipCountLabel: UILabel!
+    private(set) var flipCount = 0 {
+        didSet {
+            updateFlipCountLabel()
+        }
+        
+    }
     
-    @IBOutlet var cardButtons: [UIButton]!
+    private func updateFlipCountLabel() {
+        let attributes: [NSAttributedString.Key:Any] = [
+            .strokeWidth : 5.0,
+            .strokeColor : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        ]
+        let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+        flipCountLabel.attributedText = attributedString
+    }
     
+    @IBOutlet private weak var flipCountLabel: UILabel! {
+        didSet {
+            updateFlipCountLabel()
+        }
+    }
     
-    
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBOutlet private var cardButtons: [UIButton]!
+
+    @IBAction private func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.firstIndex(of: sender) {
             game.chooseCard(at: cardNumber)
@@ -30,7 +50,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func updateViewFromModel() {
+    private func updateViewFromModel() {
         for index in cardButtons.indices {
             let button = cardButtons[index]
             let card = game.cards[index]
@@ -43,14 +63,14 @@ class ViewController: UIViewController {
             }
         }
     }
-    var emojiChoices = ["🎃","👻","🦇","🙀","😈","🍭","🍎","🍬", "😱"]
+    private var emojiChoices = ["🎃","👻","🦇","🙀","😈","🍭","🍎","🍬", "😱"]
+    //private var emojiChoices = "🎃👻🦇🙀😈🍭🍎🍬😱"
     
-    var emoji = [Int:String]()
+    private var emoji = [Card:String]()
 
-    func emoji(for card: Card) -> String {
-        if emoji[card.identifier] == nil, emojiChoices.count > 0{
-            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+    private func emoji(for card: Card) -> String {
+        if emoji[card] == nil, emojiChoices.count > 0{
+            emoji[card] = String(emojiChoices.remove(at: emojiChoices.count.arc4Random))
         }
         /*
         if emoji[card.identifier] != nil {
@@ -59,9 +79,21 @@ class ViewController: UIViewController {
             return "?"
         }
          */
-        return emoji[card.identifier] ?? "?"
+        //print(emoji[card])
+        return emoji[card] ?? "?"
         
     }
-    
+}
+
+extension Int {
+    var arc4Random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(self)))
+        } else {
+            return 0
+        }
+    }
 }
 
